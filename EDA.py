11c648af1ff367f9f1a793a76d5c1214c51b1b0f
TanguyGodat <108273581+TanguyGodat@users.noreply.github.com>
2025-05-13 12:19:10 +0200

    csv path

--- EDA.py
+++ EDA.py
@@ -6,8 +6,8 @@ def load_split(name):
     return pd.read_csv(f"./data/clean/{name}.csv", index_col=0)
 
 # Load
-df_num = load_split("numeric")
-df_cat = load_split("categorical")
-df_ord = load_split("ordinal")
+df_num = pd.read_csv("./data/clean_reclean/numerical_cleaned.csv", index_col=0)
+df_cat = pd.read_csv("./data/clean/nominal.csv", index_col=0)
+df_ord = pd.read_csv("./data/clean_reclean/ordinal_numerized_cleaned.csv", index_col=0)
 
 print(df_cat[df_cat["Electrical"].isna()].index)
